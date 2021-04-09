0e7e37e9d96b5a37c394ddd5a75cfc6bf30e3e2d
Krzysztof <qris70@gmail.com>
2021-04-09 21:22:22 +0200

    add zad1

--- src/zad1/Zbiornik.java
+++ src/zad1/Zbiornik.java
@@ -8,13 +8,32 @@ package zad1;
 
 
 public class Zbiornik {
+
+    private int stanWody = 0;
+    private int pojemnosc;
+
     public Zbiornik(int i) {
-
+        pojemnosc = i;
     }
 
-    public void dolej(int i) {
+    public Zbiornik(Zbiornik z) {
+        pojemnosc = z.pojemnosc;
+        stanWody  = z.stanWody;
     }
 
-    public void odlej(int i) {
+    public void dolej(int woda) {
+        stanWody += woda;
+        if (stanWody > pojemnosc) {
+            stanWody = pojemnosc;
+        }
     }
-}
+
+    public void odlej(int woda) {
+            stanWody -= woda;
+            if (stanWody < 0) {stanWody = 0;}
+        }
+    public String toString()  {
+        return "Zbiornik nr "+z+", pojemnosc "+pojemnosc+", stan wody "+stanWody;
+    }
+    }
+
