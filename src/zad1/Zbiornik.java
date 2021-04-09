838a74d0d2bca1f6c5305fd2c6de3d07ecff80cc
Krzysztof <qris70@gmail.com>
2021-04-09 21:44:07 +0200

    add zad1 - class Zbiornik

--- src/zad1/Zbiornik.java
+++ src/zad1/Zbiornik.java
@@ -9,31 +9,46 @@ package zad1;
 
 public class Zbiornik {
 
-    private int stanWody = 0;
-    private int pojemnosc;
+    private double pojemnosc=0;
+    private double stanWody=0;
+    private static int lot =0;
+    private int number=0;
 
-    public Zbiornik(int i) {
-        pojemnosc = i;
+    public Zbiornik() {
+        number=++lot;
+    }
+
+    public Zbiornik(double y, double x) {
+        pojemnosc = y;
+        stanWody = x;
+        number=++lot;
+    }
+
+    public Zbiornik(double x) {
+        this(x,0);
     }
 
     public Zbiornik(Zbiornik z) {
-        pojemnosc = z.pojemnosc;
-        stanWody  = z.stanWody;
+        pojemnosc=z.pojemnosc;
+        stanWody=z.stanWody;
+        number=++lot;
     }
 
-    public void dolej(int woda) {
-        stanWody += woda;
-        if (stanWody > pojemnosc) {
-            stanWody = pojemnosc;
-        }
+    public void dolej(double water) {
+        stanWody+=water;
+        if (stanWody>pojemnosc) {stanWody=pojemnosc;}
+
+    }
+    public void odlej(double water) {
+        stanWody-=water;
+        if (stanWody<0) {stanWody=0;}
     }
 
-    public void odlej(int woda) {
-            stanWody -= woda;
-            if (stanWody < 0) {stanWody = 0;}
-        }
     public String toString()  {
-        return "Zbiornik nr "+z+", pojemnosc "+pojemnosc+", stan wody "+stanWody;
+        return "Zbiornik nr "+number+", pojemnosc "+pojemnosc+", stan wody "+stanWody;
     }
+    public void show() {
+        System.out.println(toString());
     }
 
+}
