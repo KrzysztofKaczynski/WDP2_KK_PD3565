93b0403bac081f615477ebad859572d7fbbd7d10
Krzysztof <qris70@gmail.com>
2021-04-16 07:10:01 +0200

    zad 4

---
+++ src/zad2/Test.java
@@ -0,0 +1,33 @@
+package zad2;
+/*
+public class Test {
+    public static void main(String[] args)
+    {
+        Person p1 = new Person("kris"), p2 = new Person("ewa");
+        Account  acc1 = new Account(), acc2 = new Account();
+
+        BankCustomer kris = new BankCustomer(p1);
+        BankCustomer ewa = new BankCustomer(p2);
+
+        kris.getAccount().deposit();
+
+        acc1.deposit(500);
+        acc2.deposit(900);
+
+        acc2.withdraw(300);
+        acc1.transfer(acc2, 200);
+
+        System.out.println(acc1);
+        System.out.println(acc2);
+
+        acc2.transfer(acc1, 950);
+
+        Account.setInterestRate(2);
+        acc1.addInterest();
+        acc2.addInterest();
+
+        System.out.println(acc1);
+        System.out.println(acc2);
+    }
+}
+*/
